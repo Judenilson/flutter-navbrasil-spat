de3c80ef82e65a09b1d874838258ee559c121e63
judenilson <judenilson@hotmail.com>
2023-11-10 23:22:54 -0300

    implemeted MyColors

---
+++ lib/commom/mycolors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color blueDark = Color(0xFF0A6D92);
+  static const Color blueTopGradient = Color(0xFF00ADFA);
+  static const Color blueBottomGradient = Color(0xFFBFF9FF);
+  static const Color baseBackground = Color(0xFFedeff3);
+  static const Color topBackground = Color(0xFFf2f4f8);
+  static const Color blueRed = Color(0xff4e39f9);
+  static const Color lightGreen = Color(0xFFC2FACC);
+  static const Color lightRed = Color(0xFFFFACAC);
+  static const Color lightBlue = Color(0xFFd2d1f6);
+  static const Color lightPink = Color(0xFFF4D0E1);
+  static const Color mediumBlue = Color(0xFF897dee);
+  static const Color mediumPink = Color(0xFFF478A1);
+
+}
