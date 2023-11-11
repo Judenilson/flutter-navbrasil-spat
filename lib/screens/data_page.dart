fca094de455a7b720cf89e2a209f5147524b94d2
judenilson <judenilson@hotmail.com>
2023-11-11 20:15:51 -0300

    Loading CSV

--- lib/screens/data_page.dart
+++ lib/screens/data_page.dart
@@ -1,11 +1,39 @@
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:navbrasil_spat/screens/sector_page.dart';
 
-class DataPage extends StatelessWidget {
+class DataPage extends StatefulWidget {
   const DataPage({super.key});
 
   @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  Widget? bd;
+  bool loadingStatus = false;
+  List<List<dynamic>> data = [];
+
+  void loadCSV() async {
+    final rawData = await rootBundle.loadString('assets/data.CSV');
+    List<List<dynamic>> listData =
+        const CsvToListConverter(fieldDelimiter: ';').convert(rawData);
+    // debugPrint(data.toString());
+    data = listData;
+    // debugPrint(listData.length as String?);
+    // for (int i =0; i< listData.length)
+
+    setState(() {
+      loadingStatus = true;
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    !loadingStatus ? loadCSV() :  null;
+
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.all(24),
@@ -28,11 +56,12 @@ class DataPage extends StatelessWidget {
             ),
             const Spacer(),
             ElevatedButton.icon(
-              onPressed: () {
+              onPressed: !loadingStatus ? null : () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const SectorPage(),
+                    settings: RouteSettings(arguments: data),
                   ),
                 );
               },
