dc6826e1b1dbe70ac2d5a286c66f10c444aa026a
judenilson <judenilson@hotmail.com>
2023-11-12 19:36:25 -0300

    equipment page updated

--- lib/screens/equipment_page.dart
+++ lib/screens/equipment_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navbrasil_spat/commom/mycolors.dart';
+import 'package:navbrasil_spat/models/equipment.dart';
 
 class EquipmentPage extends StatefulWidget {
   const EquipmentPage({super.key});
@@ -8,44 +10,141 @@ class EquipmentPage extends StatefulWidget {
 }
 
 class _EquipmentPageState extends State<EquipmentPage> {
+  void verifiedStatus(Equipment equipment) {
+    (equipment.state == "OK") ? equipment.state = "" : equipment.state = "OK";
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          title: Column(
+    final Equipment equipment =
+        ModalRoute.of(context)!.settings.arguments as Equipment;
+
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [MyColors.topBackground, MyColors.baseBackground],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Text(
-                'NAV Brasil - SPAT',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+                'EQUIPAMENTO',
               ),
-              Text(
-                'Rádio',
-                style: TextStyle(
-                  color: Colors.lightGreenAccent[400],
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
+              Image.asset(
+                'assets/images/nav_logo.png',
+                fit: BoxFit.contain,
+                height: 36,
               ),
             ],
           ),
           centerTitle: true,
-          backgroundColor: Colors.blueGrey,
           elevation: 0,
-          toolbarHeight: 72,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(32),
-            ),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(12),
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(width: 1, color: Colors.black12),
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  equipment.image,
+                ),
+              ),
+              Container(
+                height: 20,
+              ),
+              Text(
+                equipment.name,
+                style:
+                    const TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
+              ),
+              const Divider(),
+              Text(
+                'Patrimônio: ${equipment.id}',
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              const Divider(),
+              Text(
+                'Localização: ${equipment.location}',
+                style: const TextStyle(fontSize: 18),
+                textAlign: TextAlign.right,
+              ),
+              const Divider(),
+              Text(
+                'Observações: ${equipment.description}',
+                style: const TextStyle(fontSize: 18),
+                textAlign: TextAlign.right,
+              ),
+              const Spacer(),
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.settings),
+                    label: const Text('Editar'),
+                    style: const ButtonStyle(
+                      foregroundColor: MaterialStatePropertyAll(Colors.black),
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      verifiedStatus(equipment);
+                    },
+                    icon: const Icon(Icons.check_circle),
+                    label: (equipment.state == "OK")
+                        ? const Text('Perder')
+                        : const Text('Verificar'),
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStatePropertyAll(
+                          (equipment.state == "OK")
+                              ? MyColors.mediumRed
+                              : MyColors.navGreen),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.verified,
+                        color: (equipment.state == "OK")
+                            ? MyColors.navLightGreen
+                            : MyColors.mediumRed,
+                        size: 48,
+                      ),
+                      (equipment.state != "OK")
+                          ? const Text(
+                              "Não Localizado",
+                              style: TextStyle(
+                                  fontSize: 10, color: MyColors.mediumRed),
+                            )
+                          : const Text(
+                              "Verificado",
+                              style: TextStyle(
+                                  fontSize: 10, color: MyColors.navLightGreen),
+                            ),
+                    ],
+                  ),
+                ],
+              )
+            ],
           ),
         ),
-        Image.asset(
-          'assets/images/img1.jpg',
-        ),
-        const Text("text"),
-      ],
+      ),
     );
   }
 }
